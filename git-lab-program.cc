8683bbf9bc42d17239a842ccab2e6033aa1f3436
tristan <tg884922@ohio.edu>
2024-01-24 19:15:45 -0500

    yay

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tristan Gilles
+ *        Date: Jan 24, 2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+    
+}// main
